30f3b7101a24f7e81635e85c3f8cc5ee330d401d
Matheus Fidelis <matheusfidelis@yahoo.com>
2024-05-29 14:18:18 -0300

    build: Added Angular Material

--- frontend/src/app/pages/posts/posts-read/posts-read.component.ts
+++ frontend/src/app/pages/posts/posts-read/posts-read.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Post } from '../posts-data/posts-data.component';
 import { PostService } from '../post-service/post.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-posts-read',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './posts-read.component.html',
   styleUrl: './posts-read.component.scss',
 })
