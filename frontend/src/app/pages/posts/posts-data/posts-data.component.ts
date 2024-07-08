340cbc5a0911d73c1aeccfd8ff2e4427506587fb
Matheus Fidelis <matheusfidelis@yahoo.com>
2024-07-08 11:47:35 -0300

    Feat: Added edit method in post

--- frontend/src/app/pages/posts/posts-data/posts-data.component.ts
+++ frontend/src/app/pages/posts/posts-data/posts-data.component.ts
@@ -11,22 +11,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { Post } from 'src/app/core/models/post';
 import { PostService } from 'src/app/core/service/post-service/post.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-posts-data',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, MatCardModule],
   templateUrl: './posts-data.component.html',
   styleUrl: './posts-data.component.scss',
 })
 export class PostsDataComponent implements OnInit {
   form = new FormGroup({
+    author: new FormControl(''),
     title: new FormControl(''),
     content: new FormControl(''),
-    date: new FormControl(''),
-    theme: new FormControl(''),
+    subject: new FormControl(''),
     summary: new FormControl(''),
-    author: new FormControl(''),
+    date: new FormControl(''),
   });
 
   submitted = false;
@@ -41,21 +42,29 @@ export class PostsDataComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      title: ['', Validators.required],
       author: ['', Validators.required],
+      title: ['', Validators.required],
       content: ['', Validators.required],
-      date: ['', Validators.required],
+      subject: ['', Validators.required],
       summary: ['', Validators.required],
-      theme: ['', Validators.required],
+      date: [this.formatDate(new Date()), Validators.required],
     });
 
     if (this.route.snapshot.paramMap.get('id')) {
       this.title = 'Editar Post';
 
       const id = this.route.snapshot.paramMap.get('id');
-      // this.customerService.readById(id!).subscribe((customer) => {
-      //   this.customer = customer;
-      // });
+
+      this.postService.getById(id!).subscribe((post) => {
+        this.form.patchValue({
+          author: post.author,
+          title: post.title,
+          content: post.content,
+          subject: post.subject,
+          summary: post.summary,
+          date: this.formatDate(post.date),
+        });
+      });
     } else {
       this.title = 'Novo Post';
     }
@@ -68,32 +77,64 @@ export class PostsDataComponent implements OnInit {
   onSubmit(): void {
     this.submitted = !this.submitted;
 
-    const formValues = this.form.value;
-
-    debugger;
-
-    const newPost: Post = {
-      title: formValues.title!,
-      content: formValues.content!,
-      date: formValues.date!,
-      subject: formValues.theme!,
-      summary: formValues.summary!,
-      author: formValues.author!,
-    };
-
     if (this.form.invalid) {
       return;
     }
 
-    this.postService.create(newPost).subscribe(() => {
-      this.router.navigate(['/posts']);
-    });
+    const formValues = this.form.value;
+    const dateNow = new Date();
 
-    console.log(JSON.stringify(this.form.value, null, 2));
+    const newPost: Post = {
+      author: formValues.author!,
+      title: formValues.title!,
+      content: formValues.content!,
+      subject: formValues.subject!,
+      summary: formValues.summary!,
+      date: this.parseDate(dateNow),
+    };
+
+    if (this.route.snapshot.paramMap.get('id')) {
+      let id = this.route.snapshot.paramMap.get('id');
+      this.postService.update(id, newPost).subscribe(() => {
+        this.router.navigate(['/posts']);
+      });
+    } else {
+      this.postService.create(newPost).subscribe(() => {
+        this.router.navigate(['/posts']);
+      });
+    }
   }
 
   onReset(): void {
     this.submitted = false;
     this.form.reset();
+  }
+
+  get titleAttr() {
+    return this.form.get('title');
+  }
+
+  formatDate(dateString: any): string {
+    const date = new Date(dateString);
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  }
+
+  parseDate(dateString: any) {
+    const [day, month, year, hours, minutes, seconds] = [
+      dateString.getDate().toString().padStart(2, '0'),
+      (dateString.getMonth() + 1).toString().padStart(2, '0'),
+      dateString.getFullYear(),
+      dateString.getHours().toString().padStart(2, '0'),
+      dateString.getMinutes().toString().padStart(2, '0'),
+      dateString.getSeconds().toString().padStart(2, '0'),
+    ];
+
+    const date = `${day}-${month}-${year}`;
+    const time = `${hours}:${minutes}:${seconds}`;
+
+    return `${date} ${time}`;
   }
 }
